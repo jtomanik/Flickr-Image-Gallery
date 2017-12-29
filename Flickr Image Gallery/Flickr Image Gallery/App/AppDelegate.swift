7f212ab2079bd72307a8b166b991d360c80729dd
Jakub Tomanik <j.tomanik@gmail.com>
2017-12-29 21:20:45 +0100

    Removed unnecesary files and reorganized remining files (#5)

---
+++ Flickr Image Gallery/Flickr Image Gallery/App/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Flickr Image Gallery
+//
+//  Created by Jakub Tomanik on 27/12/2017.
+//  Copyright © 2017 Jakub Tomanik. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
