7584fa3ca0fa9550d8adb30b7ed3d54fe2d18291
Jakub Tomanik <j.tomanik@gmail.com>
2017-12-30 19:42:05 +0100

    UIKit Independent display models

---
+++ Flickr Image Gallery/Flickr Image Gallery/App/PhotoDisplayModel.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoDisplayModel.swift
+//  Flickr Image Gallery
+//
+//  Created by Jakub Tomanik on 30/12/2017.
+//  Copyright © 2017 Jakub Tomanik. All rights reserved.
+//
+
+import Foundation
+
+/// UIKit independent abstraction of view that will be showing photo details
+struct PhotoDisplayModel {
+    let url: URL
+}
