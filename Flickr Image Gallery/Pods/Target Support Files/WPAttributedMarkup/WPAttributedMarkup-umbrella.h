b4c185a9ac3be7ae0f34a2a5c14600af52cfa424
Jakub Tomanik <j.tomanik@gmail.com>
2018-01-02 05:37:50 +0100

    Added new pod `WPAttributedMarkup`

---
+++ Flickr Image Gallery/Pods/Target Support Files/WPAttributedMarkup/WPAttributedMarkup-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "NSMutableString+TagReplace.h"
+#import "NSString+WPAttributedMarkup.h"
+#import "WPAttributedStyleAction.h"
+#import "WPHotspotLabel.h"
+#import "WPTappableLabel.h"
+
+FOUNDATION_EXPORT double WPAttributedMarkupVersionNumber;
+FOUNDATION_EXPORT const unsigned char WPAttributedMarkupVersionString[];
+
