246a6addf37cc964de09ebd1f54b6f8403f9910e
Jakub Tomanik <j.tomanik@gmail.com>
2018-01-02 15:25:05 +0100

    Added Gateway / Repository definitions

--- Flickr Image Gallery/Flickr Image Gallery/App/BaseGateway.swift
+++ Flickr Image Gallery/Flickr Image Gallery/App/BaseGateway.swift
@@ -11,7 +11,14 @@ import RxSwift
 
 protocol BaseGateway: class {}
 
-/// Abstraction to hide implementation details and prevent tight coupling between Presenter and DataSource
+/// The Gateway
+/// An abstraction that encapsulates the semantic gap between the object-oriented domain layer represented by the Use Cases
+/// and the relation-oriented persistence layer represented by the Repositiories.
+///
+/// The Repositiory
+/// A Repository mediates between the data source and the business domain of the application.
+/// It queries the data source for the data, maps the data from the data source to a business entity, and persists changes in the business entity to the data source.
+/// A repository separates the business logic contained in the Use Cases from the interactions with the underlying data source or Web service.
 protocol PhotoFeedGateway: BaseGateway {
     func getPublicFeed() -> Observable<[PhotoItem]>
 }
